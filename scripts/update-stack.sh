dd7925510f3c672ff29d3590539af4b6c9b7b6b7
Adam Lupinacci <adam@Adams-MacBook-Pro.local>
2019-02-19 20:07:43 -0800

    Fixed update stack script

--- scripts/update-stack.sh
+++ scripts/update-stack.sh
@@ -11,4 +11,5 @@ aws s3 cp $ARTIFACTDIR/$ARTIFACTNAME s3://$S3BUCKET/$ARTIFACTNAME
 echo Deploying artifact...
 aws cloudformation update-stack \
     --stack-name $NAME \
-    --template-url s3://$S3BUCKET/$ARTIFACTNAME
+    --template-url https://s3.amazonaws.com/$S3BUCKET/$ARTIFACTNAME \
+    --capabilities CAPABILITY_NAMED_IAM
